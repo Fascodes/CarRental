3a3206910d6f58c6b19540012cc2ed3ba4635416
Fascodes <fasolannnn@gmail.com>
2026-06-29 19:18:51 +0200

    feature: Added a simple CI pipeline to run the modified tests on each dev/main push

--- backend/src/test/java/dev/fascodes/carRental/util/AbstractIntegrationTest.java
+++ backend/src/test/java/dev/fascodes/carRental/util/AbstractIntegrationTest.java
@@ -6,13 +6,19 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.lifecycle.Startables;
+
+import java.util.stream.Stream;
 
 @Testcontainers
 @SpringBootTest
 @ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
+
+    public static final RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3.8-management-alpine");
 
     @Container
     static PostgreSQLContainer<?> postgres =
@@ -26,5 +32,14 @@ public abstract class AbstractIntegrationTest {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.rabbitmq.host", rabbitMQContainer::getHost);
+        registry.add("spring.rabbitmq.port", rabbitMQContainer::getAmqpPort);
+
     }
+
+    static  {
+        Startables.deepStart(Stream.of(rabbitMQContainer, postgres)).join();
+    }
+
+
 }
